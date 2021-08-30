864fd2f41072ad17f62b8a1e0216ff8fd83785e5
heberhardt <hjve@h2tecnologia.com>
2021-08-30 17:25:25 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,62 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user)
+    return response.status(404).json({error : "user not found."});
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.todos.length >= 10 && user.pro === false){
+    return response.status(403).json({error: "you must have a pro plan to add more than 10 todos."});
+  }
+  
+  next();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const id = request.params.id;
+
+  if(!validate(id))
+     return response.status(400).json({error: "Could not find this todo."})
+
+  const user = users.find(user => user.username === username);
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: "Could not find this todo."})
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const id = request.params.id;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user)
+    return response.status(404).json({error : "user not found."});
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
